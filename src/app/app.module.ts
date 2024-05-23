116e9530fc31b9e5926466f32cc3f834754ec538
merveille <merveillenitcheu12@gmail.com>
2024-05-23 12:37:28 +0100

    front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { ProductService } from './demo/service/product.service';
 import { CookieService } from 'ngx-cookie-service';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS,HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TankService } from './demo/service/tank.service';
 import { ServicesStationsService } from './demo/service/servicesStations.service';
 import { CompagniesService } from './demo/service/compagnies.service';
 import { JaugeService } from './demo/service/jauge.service';
 import { ConfirmationService } from 'primeng/api';
+import { InterceptorService } from './demo/interceptors/interceptor.service';
 
 
 
@@ -32,8 +33,12 @@ import { ConfirmationService } from 'primeng/api';
         ServicesStationsService,
         CompagniesService,
         JaugeService,
-        ConfirmationService
-
+        ConfirmationService,
+        // {
+        //     provide: HTTP_INTERCEPTORS,
+        //     useClass:InterceptorService ,
+        //     multi: true
+        // }
     ],
     bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:"http://173.249.8.175:9005/api",
 };
