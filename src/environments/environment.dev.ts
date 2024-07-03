2d1ea40359780c343e4615a032e4d76173e6efca
merveille <merveillenitcheu12@gmail.com>
2024-07-03 10:08:01 +0100

    update .env

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    apiUrl:"http://173.249.8.175:8003/api",
+    authUrl: 'https://www.lightoil.cm',
+
+    pusher: {
+      key: '52f92389bf572016d550',
+      cluster: 'mt1',
+    }
+  };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl:"http://161.97.99.88:9099/api",
+  apiUrl:"http://161.97.99.88:8003/api",
   authUrl: 'https://www.lightoil.cm',
 
   pusher: {
